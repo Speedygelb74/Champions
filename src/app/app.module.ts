cd26021dca5fba35592683763d6121bde53bf6a8
Speedygelb74 <nico.heiderich@gmail.com>
2021-06-22 13:07:43 +0200

    Include champions gallery and champions card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HeaderComponent } from './templates/header/header.component';
 import { SearchComponent } from './templates/search/search.component';
+import { ChampionsGalleryComponent } from './templates/champions-gallery/champions-gallery.component';
+import { ChampionsCardComponent } from './templates/champions-card/champions-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    SearchComponent
+    SearchComponent,
+    ChampionsGalleryComponent,
+    ChampionsCardComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
