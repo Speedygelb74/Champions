8978b9024c8104812dcb6a8b3acdf434c1cbbba8
Speedygelb74 <nico.heiderich@gmail.com>
2021-06-23 17:09:06 +0200

    Add search-filter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,8 @@ import { SearchComponent } from './templates/_shared/search/search.component';
 import { ChampionsGalleryComponent } from './templates/champions-gallery/champions-gallery.component';
 import { ChampionsCardComponent } from './templates/champions-card/champions-card.component';
 import { LoaderComponent } from './templates/_shared/loader/loader.component';
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -18,12 +20,15 @@ import { LoaderComponent } from './templates/_shared/loader/loader.component';
     ChampionsGalleryComponent,
     ChampionsCardComponent,
     LoaderComponent
+    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     NgbModule,
-    HttpClientModule
+    HttpClientModule,
+    Ng2SearchPipeModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
