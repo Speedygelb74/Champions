9443668add2a012925178b8bb8828bbe56f8f6c0
Speedygelb74 <nico.heiderich@gmail.com>
2021-06-24 17:10:32 +0200

    update old path to new path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ChampionsDetailComponent } from './templates/champions-detail/champions-detail.component';
 import { ChampionsComponent } from './templates/champions/champions.component';
 
 const routes: Routes = [
-  { path: '', component: ChampionsComponent }
+  { path: '', component: ChampionsComponent },
+  { path: ':id', component: ChampionsDetailComponent }
 ];
 
 
